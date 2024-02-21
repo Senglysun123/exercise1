86b5c4850ea0d8e2c73a4a7f02eaeabdbadb5fd9
Seng Lysun <senglysun645@gmail.com>
2024-02-21 20:03:41 +0700

    second add

--- Main.java
+++ Main.java
@@ -2,23 +2,78 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Create an instance of the MyArray class
+        MyArray myArray = new MyArray();
+    }
 
-        System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
+    public static class MyArray {
+        private int[] arr;
+        private int current = 5;
+        private int index = 0;
+        private Scanner input;
 
-        int[] numbers = new int[n];
+        public MyArray() {
+            arr = new int[current];
+            input = new Scanner(System.in);
+            int n = 0;
 
-        System.out.println("Enter the numbers:");
-
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
+            do {
+                System.out.println("-------- MyArray ---------");
+                System.out.println("1. Input array");
+                System.out.println("2. View array");
+                System.out.println("0. Exit");
+                System.out.println("--------------------------");
+                System.out.print("Choose : ");
+                n = input.nextInt();
+                switch (n) {
+                    case 1:
+                        addArray();
+                        break;
+                    case 2:
+                        viewArray();
+                        break;
+                }
+            } while (n != 0);
         }
 
-        System.out.println("Input numbers:");
+        private boolean isOutOfIndex() {
+            if (index + 1 == current) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(numbers[i]);
+        private void increaseArray() {
+            if (isOutOfIndex()) {
+                int newArr[] = new int[current + 5];
+                for (int i = 0; i < current; i++) {
+                    newArr[i] = arr[i];
+                }
+                arr = newArr;
+                current += 5;
+            }
+        }
+
+        private void addArray() {
+            while (true) {
+                System.out.print("Enter number to array[" + index + "], enter -1 to exit : ");
+                int number = input.nextInt();
+                if (number == -1)
+                    break;
+                increaseArray();
+                arr[index++] = number;
+            }
+        }
+
+        private void viewArray() {
+            System.out.print("Array = [");
+            for (int i = 0; i < index; i++) {
+                if (i != 0)
+                    System.out.print(", ");
+                System.out.print(arr[i]);
+            }
+            System.out.println("]");
         }
     }
 }
